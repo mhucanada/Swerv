e8b50cfffaa3d1a7523181ef91c79a563ac4d9a3
mhucanada <mhucanada1@gmail.com>
2020-01-26 12:25:06 -0500

    Documentation Added

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,22 +1,23 @@
 import React from "react";
 import cold from "./cold.png";
 import hot from "./hot.png";
-import off from "./off.png";
 
 var mqtt = require('mqtt');
 var client = mqtt.connect('wss://test.mosquitto.org:8081');
 
 
-
+// Checks if RPi is connected, console logs message
 client.on("connect", () => {
     client.subscribe("test/temp");
-    console.log("nice");
+    console.log("Connected");
 })
 
 
+// Checks temperature input from RPi, changes image and records water saved based on flow rate if temp greater than 28 degrees 
 
 client.on("message", (topic, payload) => {
     console.log(parseFloat(payload.toString()));
+
     function whichImage() {
         if (parseFloat(payload) > 28) {
 
@@ -38,12 +39,6 @@ client.on("message", (topic, payload) => {
 
 
 
-const images = [
-    cold,
-    hot,
-]
-
-var sensorTemp = "";
 
 var imageSource = "";
 
@@ -53,45 +48,21 @@ var imageSource = "";
 
 
 
-
-
-
-const testTemp = 31;
-// var imageSource = "";
-var power = true;
 var waterSaved = 0;
 var flowRate = 30;
 
-// function whichImage() {
-//     imageSource = images[0];
-//     while (testTemp > 30) {
-//         function myFunction() { 
-//             setInterval(myTimer, 1000);
-//         }
-//         myFunction();
-//         function myTimer() {
-//             waterSaved += flowRate;
-//             document.getElementById("demo").innerHTML = waterSaved
-//         };
-//         imageSource = images[1];
-//         return imageSource;
-//     };
-//     window.onload = function savedWater(){
-//     document.getElementById("demo").innerHTML = waterSaved;
-//     };
-
-//     return imageSource;
-// };
-
-// const chooseImage = whichImage();
 
 
+// Prints current water flow rate or issues a warning if input is invalid.
+// Displays litres of water saved if given valid input and temperature is beyond threshold
 const Result = props => (
     <div className="result">
         <br></br>
         <p className="pad">Current state:</p>
         <img id="image" height="480" width="630"></img>
         <br></br>
+
+
         {props.saved &&
             <p className="result__key">The flow rate is currently at:
             <span className="result__value"> { flowRate = parseFloat(props.saved) }</span> L/s
@@ -99,8 +70,9 @@ const Result = props => (
             { console.log(flowRate) }
             </p>
         }
-        {props.error &&
-            <p className="result__key">Error:
+
+        {props.error && 
+            <p className="result__key">Warning:
             <span className="result__value"> {props.error}</span>
             </p>}
         <p>Litres of Water Saved:</p>
